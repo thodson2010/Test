73e0a68cdc64ed38f8a7943693d61a1d63a07502
thodson2010 <thodson2010@gmail.com>
2022-06-02 17:59:41 -0400

    Update to feature branch.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+
+bool my_bool = false;
+
 int test_function(int i) {
     std::cout << i << std::endl;
     return 0;
@@ -10,8 +13,8 @@ int main() {
     for (int i =0; i<5; i++){
         test_function(i);
         // This is a comment
+        my_bool = !my_bool;
     }
+    std::cout << my_bool;
     return 0;
 }
-
-
