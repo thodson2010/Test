fe09ee256bbf22719a61f538a42d8e6c753fcca7
thodson2010 <thodson2010@gmail.com>
2022-06-02 17:44:30 -0400

    Init Commit from CLion

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int test_function(int i) {
+    std::cout << i << std::endl;
+    return 0;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    for (int i =0; i<5; i++){
+        test_function(i);
+    }
+    return 0;
+}
+
+
